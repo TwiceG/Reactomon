2449677690234fa81756e6120671bbf24a2ae2df
TwiceG <g0stron0g@gmail.com>
2023-02-18 17:17:43 +0100

    new desing for landing page

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
-      <div className="App">
+    element:   
+        <>
+        <img id="main-pokemons" src="https://www.pngkit.com/png/full/160-1608486_pokemon-characters-png-download-image-pokemon-pikachu.png" alt="Main pokemons" />
         <Navbar />
-      </div>
+        </>
   }
 ]);
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css';
 export const Navbar = () => {
     return (
         <nav>
+            <a href="/"><img id="logo" src="https://www.freepnglogos.com/uploads/pokemon-logo-text-png-7.png" alt="Written Pokemon logo" /></a>
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/list-pokemons">Pokemons</a></li>
